0a5099ee28d0fb09713fb439a1e4784fdda89634
Rafael <sqerafael@2006gmail.com>
2026-03-05 23:24:00 -0300

    adicionei

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
+import { Gorditas } from "next/font/google";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
 export default nextConfig;
+ Gorditas
